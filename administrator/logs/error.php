75bed22d8956fa91d73fc92693c9d057e0765263
Cora Helm - Trans4mation <Cora.Helm@trans4mation.local>
2018-10-17 12:09:43 +0200

    first push

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-10-16 09:51:18 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-10-16T09:51:18+00:00	INFO ::1	joomlafailure	Ein leeres Passwort ist nicht zulässig
